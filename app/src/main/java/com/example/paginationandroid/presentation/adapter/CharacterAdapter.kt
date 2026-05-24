46b6b61db7512d2a64c75f5a0fb685a1cf3b1ee4
moseskamira <moses.african@gmail.com>
2026-05-24 12:03:35 +0300

    feat(character): create character info activity

--- app/src/main/java/com/example/paginationandroid/presentation/adapter/CharacterAdapter.kt
+++ app/src/main/java/com/example/paginationandroid/presentation/adapter/CharacterAdapter.kt
@@ -1,28 +1,33 @@
 package com.example.paginationandroid.presentation.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.paginationandroid.R
+import com.example.paginationandroid.databinding.ItemCharacterBinding
 
 class CharacterAdapter(
     private val items: List<String>
-) : RecyclerView.Adapter<CharacterAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<CharacterAdapter.MyViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val characterView: TextView = view.findViewById(R.id.character_text)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ItemCharacterBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return MyViewHolder(binding)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_character, parent, false)
-        return ViewHolder(view)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bindData(items[position])
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.characterView.text = items[position]
+    class MyViewHolder(
+        private val binding: ItemCharacterBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bindData(item: String) {
+            binding.characterText.text = item
+        }
     }
 
     override fun getItemCount() = items.size
