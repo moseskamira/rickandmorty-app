c4f8f02e01025b1dddabb2de68a8a9afc7e14f4a
moseskamira <moses.african@gmail.com>
2026-05-27 08:06:11 +0300

    feat(toolbar):add toolbar insets

---
+++ app/src/main/java/com/kamira/mortyverse/core/utils/AdMobConfig.kt
@@ -0,0 +1,11 @@
+package com.kamira.mortyverse.core.utils
+
+object AdMobConfig {
+    const val AD_UNIT_BANNER_ID = "ca-app-pub-2006965015577337/1847792537"
+    const val INTERSTITIAL_CHARACTER = "ca-app-pub-2006965015577337/8511757869"
+    const val ADMOB_APP_ID = "ca-app-pub-2006965015577337~6072462637"
+
+    const val BANNER_HOME_TEST = "ca-app-pub-3940256099942544/6300978111"
+    const val INTER_CHAR = "ca-app-pub-3940256099942544/1033173712"
+
+}
